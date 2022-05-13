ecd5b65beea5635a3bf95fe7e6bd7b3ee0416713
imouahrani <imeneouahrani@outlook.fr>
2022-05-13 09:22:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-send-static-data-to-service-test-56estv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-send-static-data-to-service-test-56estv)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { TransportationService } from './transportation.service';
+import { Car } from './car';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+export class AppComponent  {
+
+cars: Car[];
+
+  constructor (private transportationService: TransportationService) {
+  this.cars = this.transportationService.getCars();
+  }
+
+  addCar(){
+    const newCar: Car = {make: "Tesla", model: "Model X", miles: 100 };
+    this.transportationService.addCar(newCar);
+  }
+  
+}
